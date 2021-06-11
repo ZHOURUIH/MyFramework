cf0edc4e9ed90b76ba8711994662ee0f3d635b19
zhourui <785130190@qq.com>
2021-06-11 23:37:19 +0800

    整理代码

--- Assets/Scripts/Game/Common/GameDefine.cs
+++ Assets/Scripts/Game/Common/GameDefine.cs
@@ -11,16 +11,4 @@ public class GameDefine : FrameDefine
 	//-----------------------------------------------------------------------------------------------------------------
 	// 标签
 	// 层
-	// 清理时需要保留的目录和目录的meta
-	public static string[] mKeepFolder = new string[] 
-	{
-		"Config", "Video", "Version", "PathKeyFrame", "DataBase"
-	};
-	// Resources下的目录,带相对路径,且如果前缀符合,也会认为是不打包的目录
-	// 已经打包图集,并且没有被引用的图片也不打包AB
-	public static string[] mUnPackFolder = new string[]
-	{
-		"Texture/GameTexture/Unpack/",
-		"Texture/NumberStyle/",
-	};
 }
