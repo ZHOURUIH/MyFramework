3da7065f0b400cce14646831c54d7be78e731346
zhourui <785130190>
2026-06-27 00:35:08 +0800

    完善全局配置表格代码生成

--- Assets/Scripts/HotFix/DataBase/Excel/Data/EDGlobal.cs
+++ Assets/Scripts/HotFix/DataBase/Excel/Data/EDGlobal.cs
@@ -2,10 +2,23 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using static StringUtility;
 
 // 全局变量表格
 public class EDGlobal : ExcelData
 {
+	public const int TEST_GLOBAL_0 = 10;			// 测试全局参数0
+	public const float TEST_GLOBAL_1 = 3.5f;		// 测试全局参数1
+	public const long TEST_GLOBAL_2 = 99999999999;	// 测试全局参数2
+	public static Vector2Int TEST_GLOBAL_3;			// 测试全局参数3
+	public static Vector2 TEST_GLOBAL_4;			// 测试全局参数4
+	public static Vector3 TEST_GLOBAL_5;			// 测试全局参数5
+	public static Vector3Int TEST_GLOBAL_6;			// 测试全局参数6
+	public static List<int> TEST_GLOBAL_7;			// 测试全局参数7
+	public static List<float> TEST_GLOBAL_8;		// 测试全局参数8
+	public static List<long> TEST_GLOBAL_9;			// 测试全局参数9
+	public static string TEST_GLOBAL_10;			// 测试全局参数10
+
 	public string mParamName;						// 参数名
 	public string mParamType;						// 参数类型
 	public string mParamValue;						// 参数值
@@ -17,6 +30,20 @@ public class EDGlobal : ExcelData
 		result = result && reader.readString(out mParamValue);
 		return result;
 	}
-	public static void postLoadAll(ExcelTableT<EDGlobal> table){}
+	public static void postLoadAll(ExcelTableT<EDGlobal> table)
+	{
+		using var a = new DicScope<string, string>(out var paramMap);
+		foreach (EDGlobal data in table.queryAll())
+		{
+			paramMap.add(data.mParamName, data.mParamValue.removeAllEmpty());
+		}
+		TEST_GLOBAL_3 = SToV2I(paramMap["TEST_GLOBAL_3"]);
+		TEST_GLOBAL_4 = SToV2(paramMap["TEST_GLOBAL_4"]);
+		TEST_GLOBAL_5 = SToV3(paramMap["TEST_GLOBAL_5"]);
+		TEST_GLOBAL_6 = SToV3I(paramMap["TEST_GLOBAL_6"]);
+		TEST_GLOBAL_7 = SToIs(paramMap["TEST_GLOBAL_7"]);
+		TEST_GLOBAL_8 = SToFs(paramMap["TEST_GLOBAL_8"]);
+		TEST_GLOBAL_9 = SToLs(paramMap["TEST_GLOBAL_9"]);
+	}
 }
 // auto generate end
--- Assets/Scripts/HotFix/DataBase/Excel/Table/ExcelGlobal.cs
+++ Assets/Scripts/HotFix/DataBase/Excel/Table/ExcelGlobal.cs
@@ -1,35 +1,13 @@
-﻿// auto generate start
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using static StringUtility;
 
 public class ExcelGlobal : ExcelTableT<EDGlobal>
 {
-	public const int TEST_GLOBAL_0 = 10;							// 测试全局参数0
-	public const float TEST_GLOBAL_1 = 3.5f;						// 测试全局参数1
-	public const long TEST_GLOBAL_2 = 99999999999;					// 测试全局参数2
-	public static Vector2Int TEST_GLOBAL_3;							// 测试全局参数3
-	public static Vector2 TEST_GLOBAL_4;							// 测试全局参数4
-	public static Vector3 TEST_GLOBAL_5;							// 测试全局参数5
-	public static Vector3Int TEST_GLOBAL_6;							// 测试全局参数6
-	public static List<int> TEST_GLOBAL_7;							// 测试全局参数7
-	public static List<float> TEST_GLOBAL_8;						// 测试全局参数8
-	public static List<long> TEST_GLOBAL_9;							// 测试全局参数9
-	public static string TEST_GLOBAL_10;							// 测试全局参数10
-	protected override void onOpenFile()
+    // auto generate start
+	protected override void checkAllDataDefault() {}
+	protected override void postParseFile()
 	{
-		using var a = new DicScope<string, string>(out var paramMap);
-		foreach (EDGlobal data in queryAll())
-		{
-			paramMap.add(data.mParamName, data.mParamValue.removeAllEmpty());
-		}
-		TEST_GLOBAL_3 = SToV2I(paramMap["TEST_GLOBAL_3"]);
-		TEST_GLOBAL_4 = SToV2(paramMap["TEST_GLOBAL_4"]);
-		TEST_GLOBAL_5 = SToV3(paramMap["TEST_GLOBAL_5"]);
-		TEST_GLOBAL_6 = SToV3I(paramMap["TEST_GLOBAL_6"]);
-		TEST_GLOBAL_7 = SToIs(paramMap["TEST_GLOBAL_7"]);
-		TEST_GLOBAL_8 = SToFs(paramMap["TEST_GLOBAL_8"]);
-		TEST_GLOBAL_9 = SToLLs(paramMap["TEST_GLOBAL_9"]);
+		EDGlobal.postLoadAll(this);
 	}
+    // auto generate end
 }
-// auto generate end
