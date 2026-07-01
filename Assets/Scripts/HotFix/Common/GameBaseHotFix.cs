2db947ff0aca61f7866003ad8e3ae51f111431ad
zhourui <785130190>
2026-07-01 12:11:46 +0800

    更新表格检查工具

--- Assets/Scripts/HotFix/Common/GameBaseHotFix.cs
+++ Assets/Scripts/HotFix/Common/GameBaseHotFix.cs
@@ -3,6 +3,8 @@
 // 因为使用很频繁所以简写为GBH,全称为GameBaseHotFix
 public class GBR
 {
+	// auto generate SQLite start
+	// auto generate SQLite end
 	// FrameSystem
 	public static NetManager mNetManager;
 	public static DemoSystem mDemoSystem;
@@ -10,9 +12,9 @@ public class GBR
 
     // 需要添加auto generate Excel start和auto generate Excel end才会自动生成代码
     // auto generate Excel start
-    public static ExcelAchivement mExcelAchivement;
-    public static ExcelGlobal mExcelGlobal;
-    public static ExcelTest mExcelTest;
+	public static ExcelAchivement mExcelAchivement;
+	public static ExcelGlobal mExcelGlobal;
+	public static ExcelTest mExcelTest;
     // auto generate Excel end
 
     // 需要添加auto generate LayoutScript start和auto generate LayoutScript end才会自动生成代码
